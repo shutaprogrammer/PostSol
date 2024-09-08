09149913b648338f81448268d077e9c1034504ce
shutaprogrammer <shuta1107123614@gmail.com>
2024-09-08 13:12:05 +0800

    新規投稿作成画面

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\TopController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
