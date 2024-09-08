1ab399c4275c6672bfd6b776b1ffb2c4e2493397
Ryuki1981 <ryuki1010tr@icloud.com>
2024-09-08 18:25:17 +0800

    新規登録と同時にステータスレコード作成
    表示もできる。

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
+use Illuminate\Support\Facades\Auth;
 
 class SubscriptionController extends Controller
 {
     public function index()
     {
-        // subscription1ページのビューを返す処理
-        return view('mypages.subscription1');
+        // 現在ログインしているユーザーのIDを取得
+        $userId = Auth::id();
+
+        // user_idが現在ログインしているユーザーのIDのstatusレコードを取得
+        $status = Status::where('user_id', $userId)->first();
+
+        // subscription1ページのビューとステータスを返す処理
+        return view('mypages.subscription1', ['status' => $status]);
     }
 }
