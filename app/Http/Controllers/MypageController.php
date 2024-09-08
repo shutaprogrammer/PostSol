56d39fe8cd84e4522f96944c9ee95b2e301555aa
Ryuki1981 <ryuki1010tr@icloud.com>
2024-09-08 15:35:31 +0800

    マイページにてプロフィール表示
    imgはまだです。
    ブックマークした投稿の一覧表示などもまだです。

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MypageController extends Controller
 {
@@ -10,5 +11,14 @@ class MypageController extends Controller
     {
         // マイページのビューを返す処理
         return view('mypages.mypage');
+    }
+
+    public function show()
+    {
+        // ログインしているユーザーの情報を取得
+        $user = Auth::user();
+
+        // mypage.blade.php にユーザー情報を渡す
+        return view('mypages.mypage', compact('user'));
     }
 }
--- resources/views/mypages/mypage.blade.php
+++ resources/views/mypages/mypage.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.app_original')
 @section('content')
-    <div>初回プロフィール作成後ここに遷移。マイページだよ</div>
+    <section>
+        <h1>プロフィール</h1>
+        <p>{{ $user->name }}</p>
+        <p>{{ $user->birth }}, {{ $user->country }}, {{ $user->prefecture }}, {{ $user->city }}, {{ $user->job }}</p>
+        <p>BM総獲得数:?、いいね総獲得数:?、保有BMコイン数:?</p>
+    </section>
+    <section>
+        <h2>ブックマークした投稿</h2>
+        <p>ブックマークした数:?</p>
+        
+    </section>
 @endsection
