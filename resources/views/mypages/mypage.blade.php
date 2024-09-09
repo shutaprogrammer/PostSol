89f6e06de4c03a79b6ef7018c77220c18bef3ee1
Ryuki1981 <ryuki1010tr@icloud.com>
2024-09-09 16:44:15 +0800

    マイページに画像表示、BM,like数の表示(途中)

--- resources/views/mypages/mypage.blade.php
+++ resources/views/mypages/mypage.blade.php
@@ -4,9 +4,12 @@
 <a href="{{ route('posts.index') }}">投稿一覧表示画面へ</a>
     <section>
         <h1>プロフィール</h1>
-        {{-- <p>{{ $user->name }}</p>
-        <p>{{ $user->birth }}, {{ $user->country }}, {{ $user->prefecture }}, {{ $user->city }}, {{ $user->job }}</p> --}}
-        <p>BM総獲得数:?、いいね総獲得数:?、保有BMコイン数:?</p>
+        @if($user->img)
+        <img src="{{ Storage::url('imgs/' .$user->img) }}" alt="">
+        @endif
+        <p>{{ $user->name }}</p>
+        <p>{{ $user->birth }}, {{ $user->country }}, {{ $user->prefecture }}, {{ $user->city }}, {{ $user->job }}</p>
+        <p>BM総獲得数:{{ '$totalBookmarks' }}、いいね総獲得数:{{ '$totalLikes' }}、保有BMコイン数:?</p>
     </section>
     <section>
         <h2>ブックマークした投稿</h2>
