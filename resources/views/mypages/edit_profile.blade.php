d240d3b08e9bb5500f87f8c1c22bf5e94d824f4a
Gebe-Captain <hokuto0128.21@gmail.com>
2024-09-09 21:51:56 +0900

    mypageを編集できるようにしました

---
+++ resources/views/mypages/edit_profile.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app_original')
+@section('content')
+    <div>
+        <form action="{{ route('mypages.update', ['id' => $user->id]) }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        @method('put')
+
+        <div>
+            <label for="img">アイコン</label>
+            <input type="file" name="img" id="img">
+        </div>
+
+        <div>
+            <label>性別</label>
+                <input type="radio" id="male" name="gender" value="男性">
+                <label for="male">男性</label>
+                <input type="radio" id="female" name="gender" value="女性">
+                <label for="female">女性</label>
+                <input type="radio" id='no_answer' name="gender" value="未回答">
+                <label for="no_answer">未回答</label>
+        </div>
+
+        <div>
+            <label>生まれ年</label>
+            <input type="text" name="birth">
+        </div>
+
+        <div>
+            <label>お住まいの国</label>
+            <input type="text" name="country">
+        </div>
+
+        <div>
+            <label>お住まいの都道府県/州</label>
+            <input type="text" name="prefecture">
+        </div>
+
+        <div>
+            <label>お住まいの市町村</label>
+            <input type="text" name="city">
+        </div>
+
+        <button type="submit">更新する</button>
+        </form>
+    </div>
+@endsection
