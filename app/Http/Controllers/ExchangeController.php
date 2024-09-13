0abf9e6953d9c1a79e40e89aa0de17c1bc0270f5
shutaprogrammer <shuta1107123614@gmail.com>
2024-09-13 15:45:22 +0800

    BMコイン換金　保有コイン数表示

--- app/Http/Controllers/ExchangeController.php
+++ app/Http/Controllers/ExchangeController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Coin;
 
 class ExchangeController extends Controller
 {
     function index()
     {
-        return view('mypages.exchange');
+        //BMコイン総数を計算
+        $totalCoins = Coin::where('user_id', Auth::id())->sum('amount');
+
+        return view('mypages.exchange', compact('totalCoins'));
     }
 }
