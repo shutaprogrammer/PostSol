fba4d97ae154194cd48e5475843881e45e59ff54
Ryuki1981 <ryuki1010tr@icloud.com>
2024-09-14 13:22:24 +0800

    ソフトデリート(できたりできなかったり)
    助けて

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\Post;
+
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +15,11 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            Post::where('created_at', '<', now()->subMinute())->delete();
+    })->everyMinute(); // 1分ごとに実行
+        //     Post::where('created_at', '<', now()->subMonth())->delete();
+        // })->daily(); // 毎日実行
     }
 
     /**
@@ -24,4 +31,5 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+
 }
---
+++ database/migrations/2024_09_14_040214_add_deleted_at_to_posts_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->softDeletes(); // ソフトデリートされた投稿用にdeleted_atカラムを追加
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropSoftDeletes(); // 元に戻す
+        });
+    }
+};
