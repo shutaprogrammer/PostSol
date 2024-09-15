24c5be85b6d4cfe6321d211a67322d4667f0ead7
Ryuki1981 <ryuki1010tr@icloud.com>
2024-09-15 15:05:15 +0800

    sub、bmからマイページに行く際のエラーを修正
    本当はマイページコントローラーのshowメソッドにredirect toに飛ばせばよかったものの、すでに俺と北斗でsubとbmのコントローラーにviewで返すようにしていたため、各メソッドにmypageのshowコントローラーの記述を追加。また、if文にしているがそれも本当は必要ない？？
    とりあえずエラーは解決。

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -57,39 +57,70 @@ class SubscriptionController extends Controller
             'amount' => 100,         // 100コインを追加
             ]);
 
+            // ログインしているユーザーの情報を取得
+            $user = Auth::user();
+            $freeuser= Status::where('user_id', $user->id)->where('status', 'Free')->exists();
+        if ($freeuser){
             // 現在ログインしているユーザーの投稿に対して付けられたブックマークの総数を取得
-            $totalBookmarks = Bookmark::whereIn('post_id', function($query) {
-                $query->select('id')->from('posts')->where('user_id', Auth::id());
-            })->count();
-            
-            // 現在ログインしているユーザーの投稿に対して付けられたいいねの総数を取得
-            $totalLikes = Like::whereIn('post_id', function($query) {
-                $query->select('id')->from('posts')->where('user_id', Auth::id());
-            })->count();
+        $totalBookmarks = Bookmark::whereIn('post_id', function($query) {
+            $query->select('id')->from('posts')->where('user_id', Auth::id());
+        })->count();
+        
+        // 現在ログインしているユーザーの投稿に対して付けられたいいねの総数を取得
+        $totalLikes = Like::whereIn('post_id', function($query) {
+            $query->select('id')->from('posts')->where('user_id', Auth::id());
+        })->count();
 
-            // ブックマークした投稿のIDを取得
-            $bookmarkedPostIds = Bookmark::where('user_id', Auth::id())
-            ->pluck('post_id'); 
-    
-            // ブックマークした投稿を取得
-            $bookmarkedPosts = Post::whereIn('id', $bookmarkedPostIds)->get();
-    
-            $totalbookemarkedposts = $bookmarkedPosts->count();
-            
-            // 現在ログインしているユーザーのIDを取得
-            $userId = Auth::id();
-    
-            // // user_idが現在ログインしているユーザーのIDのstatusレコードを取得
-            $status = Status::where('user_id', $userId)->first();
+        // ブックマークした投稿のIDを取得
+        $bookmarkedPostIds = Bookmark::where('user_id', Auth::id())
+        ->pluck('post_id'); 
 
-            //BMコイン総数を計算
-            $totalCoins = Coin::where('user_id', Auth::id())->sum('amount');
+        // ブックマークした投稿を取得
+        $bookmarkedPosts = Post::whereIn('id', $bookmarkedPostIds)->get();
 
-            // 更新が成功した後、mypageビューにリダイレクト
-            return view('mypages.mypage', compact('user', 'totalBookmarks', 'totalLikes', 'bookmarkedPosts', 'totalbookemarkedposts', 'status','totalCoins'));
+        $totalbookemarkedposts = $bookmarkedPosts->count();
+
+        // 現在ログインしているユーザーのIDを取得
+        $userId = Auth::id();
+
+        // // user_idが現在ログインしているユーザーのIDのstatusレコードを取得
+        $status = Status::where('user_id', $userId)->first();
+
+        //BMコイン総数を計算
+        $totalCoins = Coin::where('user_id', Auth::id())->sum('amount');
+        }else{
+            // 現在ログインしているユーザーの投稿に対して付けられたブックマークの総数を取得
+        $totalBookmarks = Bookmark::whereIn('post_id', function($query) {
+            $query->select('id')->from('posts')->where('user_id', Auth::id());
+        })->count();
+        
+        // 現在ログインしているユーザーの投稿に対して付けられたいいねの総数を取得
+        $totalLikes = Like::whereIn('post_id', function($query) {
+            $query->select('id')->from('posts')->where('user_id', Auth::id());
+        })->count();
+
+        // ブックマークした投稿のIDを取得
+        $bookmarkedPostIds = Bookmark::where('user_id', Auth::id())
+        ->pluck('post_id'); 
+
+        // ブックマークした投稿を取得
+        $bookmarkedPosts = Post::whereIn('id', $bookmarkedPostIds)->get();
+
+        $totalbookemarkedposts = $bookmarkedPosts->count();
+
+        // 現在ログインしているユーザーのIDを取得
+        $userId = Auth::id();
+
+        // // user_idが現在ログインしているユーザーのIDのstatusレコードを取得
+        $status = Status::where('user_id', $userId)->first();
+
+        //BMコイン総数を計算
+        $totalCoins = Coin::where('user_id', Auth::id())->sum('amount');
+            return view('mypages.mypage', compact('user', 'totalBookmarks', 'totalLikes', 'bookmarkedPosts', 'totalbookemarkedposts', 'status','totalCoins','freeuser'));
         }
 
         // ステータスが見つからない場合のエラーハンドリング
         return redirect()->back()->withErrors(['msg' => 'ステータスの更新に失敗しました。']);
     }
+}
 }
