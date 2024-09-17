5408b0ccca4e5c4fe5fdce70d1f64cd53c5020b6
tomominakasu@gmail.com <tomominakasu@gmail.com>
2024-09-17 11:16:36 +0800

    悪質報告受け取り完了

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,11 +15,11 @@ class AdminController extends Controller
         return view('admin.menu');
     }
 
-    public function reports(Request $request)
+    public function reports()
     {
+        $reports = Report::with('user', 'post')->get();
 
-
-        return view('admin.reports');
+        return view('admin.reports', compact('reports'));
     }
 
     public function exchange()
