8cd55299f6a1c94c30240d24443ac74ff161b19d
Gebe-Captain <hokuto0128.21@gmail.com>
2024-09-23 11:26:58 +0900

    コイン購入画面２の修正完了

--- resources/views/mypages/bmcoin1.blade.php
+++ resources/views/mypages/bmcoin1.blade.php
@@ -61,28 +61,28 @@
 
     <div>
         <a href="{{ route('bmcoin.index2', ['coin_count' => 10, 'price' => 100]) }}" class="product-link">
-            <span>100BM coin</span>
+            <span>100コイン</span>
             <span>10回分</span>
             <span>購入 ￥100円</span>
         </a>
     </div>
     <div>
         <a href="{{ route('bmcoin.index2', ['coin_count' => 20, 'price' => 200]) }}" class="product-link">
-            <span>200BM coin</span>
+            <span>200コイン</span>
             <span>20回分</span>
             <span>購入 ￥200円</span>
         </a>
     </div>
     <div>
         <a href="{{ route('bmcoin.index2', ['coin_count' => 30, 'price' => 300]) }}" class="product-link">
-            <span>300BM coin</span>
+            <span>300コイン</span>
             <span>30回分</span>
             <span>購入 ￥300円</span>
         </a>
     </div>
     <div>
         <a href="{{ route('bmcoin.index2', ['coin_count' => 40, 'price' => 400]) }}" class="product-link">
-            <span>400BM coin</span>
+            <span>400コイン</span>
             <span>40回分</span>
             <span>購入 ￥400円</span>
         </a>
